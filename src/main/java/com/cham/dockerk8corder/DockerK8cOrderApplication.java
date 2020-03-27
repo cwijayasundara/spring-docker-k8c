ca25e0d0dafeab7bf6990acbf6cb8ba9cace9762
cwijayasundara <cwijayasundara@gmail.com>
2020-03-27 17:13:26 +0000

    first commit

---
+++ src/main/java/com/cham/dockerk8corder/DockerK8cOrderApplication.java
@@ -0,0 +1,11 @@
+package com.cham.dockerk8corder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerK8cOrderApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(DockerK8cOrderApplication.class, args);
+	}
+}
